8e9c8cad84e656325c83223a9ea41012df1736ee
jose-cassios <cassios.torres.010@gmail.com>
2025-09-09 16:42:35 -0300

    upd adicionando reset depois de morrer ao apertar espaço

--- projetos/script.js
+++ projetos/script.js
@@ -2,44 +2,51 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 
+let gameOver = false;
+
 const jump = () => {
- mario.classList.add('jump');
+    if (!gameOver) {
+        mario.classList.add('jump');
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+}
 
- setTimeout(() => {
-
-   mario.classList.remove('jump');
-    
- }, 500);
-
+const resetGame = () => {
+    location.reload();
 }
 
 const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
 
-   console.log('loop');
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-   const pipePosition = pipe.offsetLeft;
-   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-   const cloudsPosition = clouds.offsetLeft;
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
 
-   console.log(marioPosition);
-   if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 100 ){
-      
-      pipe.style.animation = 'none';
-      pipe.style.left = `${pipePosition}px`;
+        mario.src = './img/game-over.png';
+        mario.style.width = '80px';
+        mario.style.marginLeft = '50px';
 
-      mario.style.animation = 'none';
-      mario.style.bottom = `${marioPosition}px`;
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
 
-      mario.src = './img/game-over.png';
-      mario.style.width = '80px';
-      mario.style.marginLeft = '50px';
+        gameOver = true;
+        clearInterval(loop);
+    }
+}, 10);
 
-      clouds.style.animation = 'none';
-      clouds.style.left = `${cloudsPosition}px`;
-
-      clearInterval(loop);
-   }
-
-},10);
-
-document.addEventListener('keydown', jump);
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        if (gameOver) {
+            resetGame();
+        } else {
+            jump();
+        }
+    }
+});
